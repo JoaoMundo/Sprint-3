9d5f3348e7b4bd58fdd77b2b6c5ca044de777989
JoaoMundo <103788332+JoaoMundo@users.noreply.github.com>
2022-06-12 13:03:41 -0300

    Add files via upload

---
+++ dbprods.js
@@ -0,0 +1,41 @@
+var db = {
+    dados: [
+        {
+            titulo: 'placa de vídeo 1650 tuf',
+            categoria: 'hardware',
+            foto: 'placadevideo.jfif',
+            descricao: 'placa de vídeo com alto desempenho e preço que cabe dentro do bolso',
+            preco: 2000
+        }, {
+            titulo: 'Smartphone Samsung Galaxy S20 Fe',
+            categoria: 'Celulares',
+            foto: 'celular.jfif',
+            descricao: 'celular com desempenho medio e preço baixo',
+            preco: 2000
+        }, {
+            titulo: 'Monitor Gamer AOC Hero 23.8',
+            categoria: 'Monitores',
+            foto: 'monitor.jfif',
+            descricao: 'Monitor com melhor custo benefício feito para gaming',
+            preco: 1349.90
+        }, {
+            titulo: 'placa de vídeo 1650 tuf',
+            categoria: 'hardware',
+            foto: 'placadevideo.jfif',
+            descricao: 'placa de vídeo com alto desempenho e preço que cabe dentro do bolso',
+            preco: 2000
+        }, {
+            titulo: 'placa de vídeo 1650 tuf',
+            categoria: 'hardware',
+            foto: 'placadevideo.jfif',
+            descricao: 'placa de vídeo com alto desempenho e preço que cabe dentro do bolso',
+            preco: 2000
+        }, {
+            titulo: 'placa de vídeo 1650 tuf',
+            categoria: 'hardware',
+            foto: 'placadevideo.jfif',
+            descricao: 'placa de vídeo com alto desempenho e preço que cabe dentro do bolso',
+            preco: 2000
+        },
+    ]
+}
